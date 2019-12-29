eddbc8cf56fd2f8fd3a8864686e30bceebb929c5
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2019-12-29 11:29:42 +0100

    user schema

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// User Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Model & Export
+module.exports = User = mongoose.model("users", UserSchema);
