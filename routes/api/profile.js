d1edf09234a7ff3158db2a43901f388cf1c95420
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2019-12-30 11:27:59 +0100

    private user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Model
+const User = require("../../models/User");
 
 // @route       GET api/profile/test
 // @desc        tests profile route
@@ -7,5 +14,26 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "Profile Works" });
 });
+
+// @route       GET api/profile
+// @desc        Get current user's profile
+// @access      Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(Profile => {
+        if (!Profile) {
+          errors.noProfile = "There's no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
