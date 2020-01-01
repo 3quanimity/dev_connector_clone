44610b14b3cb058985b060d94e940338a99c3113
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-01-01 21:35:31 +0100

    add experience & education routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const passport = require("passport");
 
 // Load Validation
 const validateProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 // Load Profile Model
 const Profile = require("../../models/Profile");
@@ -151,6 +153,72 @@ router.post(
           new Profile(profileFields).save().then(profile => res.json(profile));
         });
       }
+    });
+  }
+);
+
+// @route       👷POST api/profile/experience
+// @desc        📄Add experience to profile
+// @access      🧐Private
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+// @route       👷POST api/profile/education
+// @desc        📄Add education to profile
+// @access      🧐Private
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldOfStudy: req.body.fieldOfStudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to exp array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => res.json(profile));
     });
   }
 );
---
+++ validation/education.js
@@ -0,0 +1,33 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateEducationInput(data) {
+  let errors = {};
+
+  // set to empty string if empty
+  data.school = !isEmpty(data.school) ? data.school : "";
+  data.degree = !isEmpty(data.degree) ? data.degree : "";
+  data.fieldOfStudy = !isEmpty(data.fieldOfStudy) ? data.fieldOfStudy : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
+
+  if (Validator.isEmpty(data.school)) {
+    errors.school = "School is required";
+  }
+
+  if (Validator.isEmpty(data.degree)) {
+    errors.degree = "Degree is required";
+  }
+
+  if (Validator.isEmpty(data.fieldOfStudy)) {
+    errors.fieldOfStudy = "Field of study is required";
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "From date field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
