21eb016bebd154eed6b864f73c4c63f7fcf0d4e2
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-01-01 22:34:14 +0100

    delete education & experience routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -215,11 +215,55 @@ router.post(
         description: req.body.description
       };
 
-      // Add to exp array
+      // Add to edu array
       profile.education.unshift(newEdu);
 
       profile.save().then(profile => res.json(profile));
     });
+  }
+);
+
+// @route       👷DELETE api/profile/experience/:exp_id
+// @desc        📄Delete experience from profile
+// @access      🧐Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      // Get remove index, using map & indexOf
+      const removeIndex = profile.experience.map(item=>item.id).indexOf(req.params.exp_id)
+  
+      // Splice out of array
+      profile.experience.splice(removeIndex, 1);
+
+      // Save
+      profile.save().then(profile=> res.json(profile))
+    })
+    .catch(err => res.status(404).json(err))
+  }
+);
+
+// @route       👷DELETE api/profile/education/:edu_id
+// @desc        📄Delete education from profile
+// @access      🧐Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      // Get remove index, using map & indexOf
+      const removeIndex = profile.education.map(item=>item.id).indexOf(req.params.edu_id)
+  
+      // Splice out of array
+      profile.education.splice(removeIndex, 1);
+
+      // Save
+      profile.save().then(profile=> res.json(profile))
+    })
+    .catch(err => res.status(404).json(err))
   }
 );
 
