b27bc8bec9538b49fe957206a961529555ffef2e
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-01-02 10:54:20 +0100

    create post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-// @route       GET api/posts/test
-// @desc        tests posts route
-// @access      Public
-router.get("/test", (req, res) => {
-  res.json({ msg: "Posts Works" });
-});
+// Post model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
+
+// @route       👷 POST api/posts/
+// @desc        📄 Create post
+// @access      🧐 Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name, // user, name & avatar will be pulled from user state (redux)
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
